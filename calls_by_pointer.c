f4437468f0ac7d2d82878c9e47a2dc311760a59b
divya25961 <divyasdivya6770@gmail.com>
2025-11-22 10:00:45 +0530

    Add files via upload

---
+++ calls_by_pointer.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+struct score
+{
+    float marks;
+    int rank;
+};
+void edit (struct score *s1)
+{
+    printf("before change in fn : %f\n",s1->marks);
+    s1->marks = 56.2;
+    printf("after change in fn : %f\n", s1->marks);
+}
+void main ()
+{
+    struct score s1 = {87.84, 4};
+    edit (&s1);
+    printf("after change in main %f\n", s1.marks);
+}
